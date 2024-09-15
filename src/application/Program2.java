cebc1dd07d7d3af2680aa841da965ef2af48c516
DavidT <davidt.info@gmail.com>
2024-09-15 20:02:36 -0300

    department findall

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Scanner;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,18 +10,19 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("Hello world!!");
 		System.out.println("=== TEST 1: findById =====");
 		Department department = departmentDao.findById(2);
 		System.out.println(department);
-//		Seller seller = sellerDao.findById(3);		
-//		System.out.println(seller);
 		
 		System.out.println("\n=== TEST 2: findAll =====");
+		List<Department> list = departmentDao.findAll();
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
+		
 		
 		System.out.println("\n=== TEST 3: insert =====");
 		
